770ac5583683f3faf6ce29882eab1527852f1f7f
Joel Stein <js@funkturm.net>
2019-03-20 02:12:37 +0100

    Make TWRP wipe userdata if no valid filesystem is found

--- partition.cpp
+++ partition.cpp
@@ -1987,6 +1987,7 @@ void TWPartition::Check_FS_Type() {
 	if (blkid_do_fullprobe(pr)) {
 		blkid_free_probe(pr);
 		LOGINFO("Can't probe device %s\n", Actual_Block_Device.c_str());
+		Current_File_System = "";
 		return;
 	}
 
